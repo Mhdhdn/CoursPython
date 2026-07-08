4821a0fd0849eeede6fc0c2fd9e46ad77e937688
Mehdi <mhdhdn1@gmail.com>
2026-07-08 12:00:27 +0200

    ajout commentaires et prompt

--- variable.py
+++ variable.py
@@ -1,10 +1,16 @@
+#Création de variables
 mon_texte = "je suis du texte, wow!"
 mon_nombre = 6271
 mon_nombre_virgule = 1.1100
 mon_booleen = True
 
+#Affichage du contenu des variables
 print(mon_texte)
 print(mon_nombre)
 print(mon_booleen)
 print(mon_nombre_virgule)
+
+#Récupération d'une valeur utilisateur et renvoyer via print
+prenom = input("Entrez votre nom : ")
+print ("Bienvenue,",prenom)
 
