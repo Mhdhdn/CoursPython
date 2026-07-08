589f6cbe1bec17653da4fed1afd7d77da1a46d6b
Mehdi <mhdhdn1@gmail.com>
2026-07-08 13:47:40 +0200

    operateurs logiques

--- operateurs.py
+++ operateurs.py
@@ -1,3 +1,4 @@
+#OPERATEURS ARYTMETIQUES = OPERATIONS MATHEMATIQUES
 nombre_un = 5
 nombre_deux = 10
 
@@ -6,4 +7,30 @@ la_somme_variables = nombre_un + nombre_deux
 
 la_difference = nombre_deux - nombre_un
 
-print("ABC " * 20)
+#print("ABC " * 20)
+
+#OPERATEURS DE COMPARAISONS 
+
+egalite = 10 == 11 #false
+
+difference = 10 != 11 #True
+
+superiorite = 10 > 5 #True
+
+superiorite_ou_egalite = 10 >= 5 #True
+
+inferiorite = 10 < 5 #False
+
+inferiorite_ou_egalite = 5 <= 10 #True
+
+#OPERATEURS LOGIQUES
+
+possede_carte_bibliotheque = True
+argent = 25
+age = int(input("Quel âge avez vous ?"))
+
+acces_bibliotheque = age >= 12 and possede_carte_bibliotheque
+
+acces_avec_paiement_possible_2_euros = argent >= 2 or possede_carte_bibliotheque
+
+est_mineur = not(age >= 18)
