a6134f6ce12f88a6e61d7896ccda0c0a8768a551
Mehdi <mhdhdn1@gmail.com>
2026-07-08 14:53:03 +0200

    exo1

---
+++ exo1.py
@@ -0,0 +1,5 @@
+nom_user = input("Entrez votre nom : ")
+prenom_user = input("Entrez votre prénom : ")
+age_user = int(input("Entrez votre âge : "))
+
+print("Bonjour,",prenom_user, nom_user,"vous avez",age_user,"ans!")
