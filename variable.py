120ef8caa09d21ef72c11fa6b9523f36af37bdd9
Mehdi <mhdhdn1@gmail.com>
2026-07-08 11:50:13 +0200

    hello et variable

---
+++ variable.py
@@ -0,0 +1,10 @@
+mon_texte = "je suis du texte, wow!"
+mon_nombre = 6271
+mon_nombre_virgule = 1.1100
+mon_booleen = True
+
+print(mon_texte)
+print(mon_nombre)
+print(mon_booleen)
+print(mon_nombre_virgule)
+
