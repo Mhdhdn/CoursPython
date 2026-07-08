5997ce19cdc38177f01de7f305c734205322905a
Mehdi <mhdhdn1@gmail.com>
2026-07-08 17:13:02 +0200

    exos essai bonus

--- exo2.py
+++ exo2.py
@@ -1,17 +1,36 @@
-choix_user = input("Bienvenue dans notre restaurant ! Voici la carte\n" \
+budget = 25
+prix_steak = 13
+prix_frites = 4
+prix_salade = 5
+prix_boisson = 2
+
+while budget > 0 :
+    
+    choix_user = input("Bienvenue dans notre restaurant ! Voici la carte\n" \
 "Steak - 13€\n"
 "Frites - 4€\n"
 "Salade - 5€\n"
 "Boisson - 2€\n"
 "Faites votre choix : ")
-
-match choix_user:
-    case "Steak":
-        print("Voici un steak, 13€ s'il vous plaît")
-    case "Frites":
-        print("Voici des frites, 4€ s'il vous plaît")
-    case "Steak":
-        print("Voici une salade, 5€ s'il vous plaît")
-    case "Steak":
-        print("Voici une boisson, 2€ s'il vous plaît")
+    message_retour = "Voici un %s , Bon appétit" %choix_user
+    match choix_user:
+        case "Steak":
+            choix_user = "Steak"
+            budget = budget - 13
+            print(message_retour)
+        case "Frites":
+            choix_user = "Frites"
+            budget = budget - 4
+            print(message_retour)
+        case "Salade":
+            choix_user = "Salade"
+            budget = budget - 5
+            print(message_retour)
+        case "Boisson":
+            choix_user = "Boisson"
+            budget = budget - 2
+            print(message_retour)
+    if budget <= 0 :
+        print("Vous n'avez pas assez d'argent")
+print ("Vous n'avez plus d'argent")
 
