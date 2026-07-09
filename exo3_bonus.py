d88655cb0e186280e6790fbb280e1fe9bf964b75
Mehdi <mhdhdn1@gmail.com>
2026-07-09 08:36:32 +0200

    exo3 bonus 1 réussi

--- exo3_bonus.py
+++ exo3_bonus.py
@@ -2,13 +2,13 @@ from getpass import getpass
 from random import randint
 
 
-nombre_un = randint(1,10)
-nombre_deux = input ("JOUEUR DEUX A VOTRE TOUR... DEVINEZ !!! ")
+nombre_un = randint(1,100)
+nombre_deux = int(input ("JOUEUR DEUX A VOTRE TOUR... DEVINEZ !!! "))
 while True : 
     if nombre_deux < nombre_un :
-        nombre_deux = input("TROP BAS, REESSAYEZ ")
-    if nombre_deux > nombre_un:
-        nombre_deux = input("TROP HAUT, REESSAYEZ")
-    if nombre_deux == nombre_un :
+        nombre_deux = int(input("TROP BAS, REESSAYEZ "))
+    elif nombre_deux > nombre_un:
+        nombre_deux = int(input("TROP HAUT, REESSAYEZ"))
+    elif nombre_deux == nombre_un :
         print ("FELICITATIONS...")
         break
